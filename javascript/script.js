db379d1afe6c961d2f8a88e7d630a1c5e39e0d64
VAB1998 <blanco.vincenzo@outlook.com>
2021-09-15 14:56:32 +0200

    JS Created a new instance of the object

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+//Three MAIN sections class previous_image image_slider  next_image
+//Creo un oggetto di tipo V
+let app = new Vue({
+    el: '#app',
+    data: {
+    
+    },
+    methods: {
+        
+    }
+});
